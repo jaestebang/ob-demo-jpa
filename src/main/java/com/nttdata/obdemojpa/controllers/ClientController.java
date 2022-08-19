269292bc9dbb86eaba3f49ee88b215a71937c316
jesteban <esteban8705@gmail.com>
2022-08-18 20:29:27 -0500

    CHG: Update controller

--- src/main/java/com/nttdata/obdemojpa/controllers/ClientController.java
+++ src/main/java/com/nttdata/obdemojpa/controllers/ClientController.java
@@ -27,22 +27,9 @@ public class ClientController {
         this.clientRepository = clientRepository;
     }
 
-    @GetMapping("/findById")
-    public ResponseEntity<Client> findById(@RequestBody ClientId clientId) {
-        log.info("Client DTO: " + clientId.toString());
-        Optional<Client> optionalClient = clientRepository.findById(new ClientId(clientId.getTypeDni(), clientId.getDni()));
-        return optionalClient.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
     @GetMapping("/findByTypeDni/{typeDni}/{dni}")
     public ResponseEntity<Client> findByTypeDni(@PathVariable String typeDni, @PathVariable int dni) {
         Optional<Client> optionalClient = clientRepository.findById(new ClientId(typeDni, dni));
         return optionalClient.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-    @GetMapping("/find")
-    public List<Client> findAll(){
-        log.info("find");
-        return clientRepository.findAll();
     }
 }
