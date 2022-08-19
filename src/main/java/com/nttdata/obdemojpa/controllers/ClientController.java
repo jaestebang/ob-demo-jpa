ea08621196d8a454da28df122f4e00f3ae521d63
jesteban <esteban8705@gmail.com>
2022-08-18 21:16:20 -0500

    CHG: Update imports

--- src/main/java/com/nttdata/obdemojpa/controllers/ClientController.java
+++ src/main/java/com/nttdata/obdemojpa/controllers/ClientController.java
@@ -6,10 +6,11 @@ import com.nttdata.obdemojpa.repositories.ClientRepository;
 import com.sun.istack.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpHeaders;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
